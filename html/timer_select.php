60e222d5db2b650bbad5090bed1b7d72634584ce
jacoloves <5511068t@gmail.com>
2021-01-04 23:47:33 +0900

    20210104

--- html/timer_select.php
+++ html/timer_select.php
@@ -8,8 +8,8 @@
 <body>
 <?php
     try {
-        $startTime = date('Y-m-d 00:00:00');
-        $endTime = date("Y-m-d 23:59:59", strtotime('+7 day'));
+        // $startTime = date('Y-m-d 00:00:00');
+        // $endTime = date("Y-m-d 23:59:59", strtotime('+7 day'));
 
         $dsn = 'mysql:dbname=tools;host=mysql;charset=utf8';
         $user = 'root';
@@ -17,21 +17,38 @@
         $dbh = new PDO($dsn, $user, $password);
         $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $sql = "SELECT subject,record FROM times_record WHERE created_at BETWEEN ? and ?";
+        // $sql = "SELECT subject,record FROM times_record WHERE created_at BETWEEN ? and ?";
+        $sql = "SELECT subject,record FROM times_record";
         $stmt = $dbh->prepare($sql);
-        $data[] = $startTime;
-        $data[] = $endTime;
-        $stmt->execute($data);
+        // $data[] = $startTime;
+        // $data[] = $endTime;
+        // $stmt->execute($data);
+        $stmt->execute();
 
         $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+        // $rec = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        var_dump($rec);
+
+        // foreach ($rec as $value) {
+        //     print($value["record"]);
+        // }
+
 
         print "<table border=1>";
         print "<th>科目名</th>";
         print "<th>勉強時間</th>";
-        print "<tr>" + $rec['subject'] + "</tr>";
-        print "<tr>" + $rec['record'] + "</tr>";;
-        print "</th>";
-        foreach ($rec as $value)
+        // foreach ($rec as $value) {
+        //     print "<tr>";
+        //     print $rec['subject'];
+        //     print "</tr>";
+        //     print "<tr>";
+        //     print $rec['record'];
+        //     print "</tr>";
+        // }
+
+        // print "<tr>" + $rec['subject'] + "</tr>";
+        // print "<tr>" + $rec['record'] + "</tr>";
         print "</table>";
 
         $dbh = null;
